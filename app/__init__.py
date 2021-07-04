80bc6f38263254f78231d8f5f9690763925cbbf3
nvenkate-cyber <68961147+nvenkate-cyber@users.noreply.github.com>
2021-07-04 15:41:11 -0400

    add flask-sqlalchemy support

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,39 @@
 import os
 from flask import Flask, request, render_template, send_from_directory, Response
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from flask.helpers import url_for
 from . import db
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-load_dotenv()
+# load_dotenv()
 app = Flask(__name__)
-app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
-db.init_app(app)
+# app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
+# db.init_app(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'portgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}'.format(
+    user=os.getenv('POSTGRES_USER'),
+    passwd=os.getenv('POSTGRES_PASSWORD'),
+    host=os.getenv('POSTGRES_HOST'),
+    port=5432,
+    table=os.getenv('POSTGRES_DB'))
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class UserModel(db.Model):
+    __tablename__ = 'users'
+
+    username = db.Column(db.String(), primary_key=True)
+    password = db.Column(db.String())
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f"<User {self.username}>"
 
 @app.route('/')
 def index():
