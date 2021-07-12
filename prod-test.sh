b32c090b3403b0834c5ccd61beac3a8024963e58
nvenkate-cyber <68961147+nvenkate-cyber@users.noreply.github.com>
2021-07-11 22:17:35 -0400

    deployment tests

---
+++ prod-test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Health
+if curl --location --request GET 'https://nandini.duckdns.org/health' | grep -q ''; then echo "success"
+else echo "error"
+fi
+
+# Register
+if curl -X POST -d "username=nand.v&password=1234" 'https://nandini.duckdns.org/register' | grep -q 'User nand created successfully'; then echo "success"
+else echo "error"
+fi
+
+if curl -X POST -d "username=nandini.v" 'https://nandini.duckdns.org/register' | grep -q 'Password is required'; then echo "success"
+else echo "error"
+fi
+
+# Login
+if curl -X POST -d "username=nand&password=1234" 'https://nandini.duckdns.org/login' | grep -q 'Login Successful'; then echo "success"
+else echo "error"
+fi
+
+if curl -X POST -d "username=nandini.v&password=oof" 'https://nandini.duckdns.org/login' | grep -q 'Incorrect password'; then echo "success"
+else echo "error"
+fi
