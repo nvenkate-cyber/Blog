4cfb87d16f33ee2c437fbabab32826f3e8c6816f
Nandini Venkatesh <68961147+nvenkate-cyber@users.noreply.github.com>
2021-07-14 15:42:35 -0400

    Update prod-test.sh

--- prod-test.sh
+++ prod-test.sh
@@ -2,23 +2,23 @@
 
 # Health
 if curl --location --request GET 'https://nandini.duckdns.org/health' | grep -q ''; then echo "success"
-else echo "error"
+else echo curl --location --request GET 'https://nandini.duckdns.org/health'
 fi
 
 # Register
-if curl -X POST -d "username=nand.v&password=1234" 'https://nandini.duckdns.org/register' | grep -q 'User nand created successfully'; then echo "success"
+if curl -X POST -d "username=nand.v&password=1234" 'https://nandini.duckdns.org/register' | grep -q 'User nand.v created successfully'; then echo "success"
 else echo "error"
 fi
 
-if curl -X POST -d "username=nandini.v" 'https://nandini.duckdns.org/register' | grep -q 'Password is required'; then echo "success"
+if curl -X POST -d "username=nand.v" 'https://nandini.duckdns.org/register' | grep -q 'Password is required'; then echo "success"
 else echo "error"
 fi
 
 # Login
-if curl -X POST -d "username=nand&password=1234" 'https://nandini.duckdns.org/login' | grep -q 'Login Successful'; then echo "success"
+if curl -X POST -d "username=nand.v&password=1234" 'https://nandini.duckdns.org/login' | grep -q 'Login Successful'; then echo "success"
 else echo "error"
 fi
 
-if curl -X POST -d "username=nandini.v&password=oof" 'https://nandini.duckdns.org/login' | grep -q 'Incorrect password'; then echo "success"
+if curl -X POST -d "username=nand.v&password=oof" 'https://nandini.duckdns.org/login' | grep -q 'Incorrect password'; then echo "success"
 else echo "error"
 fi
